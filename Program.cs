e414dca13cdd2fc8eb4ad995a07b7ef49b1763d9
TheWass <lord.d35ruct0@gmail.com>
2019-11-15 17:48:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Data.Odbc;
+
+namespace DBFileExtractor
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Enter connection string:");
+            string connectionString = Console.ReadLine();
+            Console.Write("Enter table name: ");
+            string tableName = Console.ReadLine();
+            Console.Write("Enter field name: ");
+            string fieldName = Console.ReadLine();
+            Console.Write("Enter where clause: ");
+            string whereClause = Console.ReadLine();
+
+            string queryString = string.Format("SELECT {0} FROM {1} WHERE {2}", fieldName, tableName, whereClause);
+
+            object data;
+
+            try
+            {
+                using (OdbcConnection conn = new OdbcConnection(connectionString))
+                {
+                    OdbcCommand cmd = new OdbcCommand(queryString, conn);
+                    conn.Open();
+                    data = cmd.ExecuteScalar();
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+            Console.Write("Enter output file name: ");
+            string fileName = Console.ReadLine();
+
+            string outputFile = Path.Combine(Path.GetTempPath(), fileName);
+
+            Console.Write("Text or Binary: ");
+            switch (Console.ReadLine().ToLower().Substring(0, 1))
+            {
+                case "b":
+                    using (FileStream file = File.Create(outputFile))
+                    {
+                        file.Write(data as byte[]);
+                    }
+                    break;
+                case "t":
+                default:
+                    File.WriteAllText(outputFile, data as string);
+                    break;
+            }
+
+            Console.WriteLine(string.Format("File written to: {0}", outputFile));
+
+            return 0;
+        }
+    }
+}
